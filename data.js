81afe24b5f16821801962fb4a114c277f36b118b
Rajgollapalli0810 <Rajgollapalli0810@users.noreply.github.com>
2026-07-02 09:35:41 +0530

    Fix hanging hearts visibility

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 // Personalize this file. Put your photos/videos/music in the assets folder,
 // then update the paths below. Example: "assets/our-photo.jpg"
 window.LOVE_SITE_DATA = {
-  assetVersion: "20260702-stats-swap",
+  assetVersion: "20260702-hanging-hearts",
   secretDate: "21102021",
   couple: {
     yourName: "Chinnu",
